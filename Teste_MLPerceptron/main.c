c6827aac0dc9fbe6b1abce2b6116312dc252dc61
Celso Junior <40507481+sup3rtemaki@users.noreply.github.com>
2020-10-28 21:58:26 -0300

    Inicio escrita teste MLP

---
+++ Teste_MLPerceptron/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* --- VARIAVEIS ---*/
+    int entradas = 256;
+    int qtd_neuronios = 200;
+    int amostras_por_digito = 5;
+    int tam_vetor_saida = 10; //10 algarismos
+    int amostras = amostras_por_digito * tam_vetor_saida;
+    float limiar = 0.0;
+    float alfa = 0.005; //taxa aprendizagem
+    float erro_tolerado - 0.5;
+
+    /* --- ARQUIVO DE AMOSTRAS DE TREINAMENTO ---*/
+    int entrada[amostras][entradas];
+    //TODO PREENCHER MATRIZ
+    return 0;
+}
