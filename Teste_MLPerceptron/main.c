ea6cef0feca1ec45f84a70821e9ed8a500d84207
Celso Junior <40507481+sup3rtemaki@users.noreply.github.com>
2020-10-29 22:44:45 -0300

    Inicio rotina leitura de arquivo p/ vetor entrada

--- Teste_MLPerceptron/main.c
+++ Teste_MLPerceptron/main.c
@@ -4,6 +4,22 @@
 int main()
 {
     /* --- VARIAVEIS ---*/
+    FILE *arq;
+
+    arq = fopen("0.txt", "r");
+
+    if (arq == NULL)
+    {
+        printf("ERRO! O arquivo não foi aberto!\n");
+    }
+    else
+    {
+        printf("O arquivo foi aberto com sucesso!\n");
+    }
+
+    char c, c_aux;
+    int lin = 0;
+    int col = 0;
     int entradas = 256;
     int qtd_neuronios = 200;
     int amostras_por_digito = 5;
@@ -11,10 +27,59 @@ int main()
     int amostras = amostras_por_digito * tam_vetor_saida;
     float limiar = 0.0;
     float alfa = 0.005; //taxa aprendizagem
-    float erro_tolerado - 0.5;
+    float erro_tolerado = 0.5;
 
     /* --- ARQUIVO DE AMOSTRAS DE TREINAMENTO ---*/
-    int entrada[amostras][entradas];
+    float entrada[amostras][entradas];
     //TODO PREENCHER MATRIZ
+    for(int i = 0; i < amostras; i++)
+    {
+        for(int j = 0; j < entradas; j++)
+        {
+            entrada[i][j] = -1.0;
+        }
+    }
+
+
+    while(c != EOF)
+    {
+        c = getc(arq);
+
+        if(c == 'p')
+        {
+            c_aux = 0;
+            lin++;
+            col = 0;
+        }
+        if(c == '-')
+        {
+            //entrada[lin][col] = -1.0;
+            col++;
+            c_aux = 0;
+        }
+        else
+        {
+            if(c == ' ')
+            {
+                c_aux = 1;
+            }
+            else
+            {
+                if((c == '1') && (c_aux == 1))
+                {
+                    entrada[lin][col] = 1.0;
+                    col++;
+                    c_aux = 0;
+                }
+            }
+        }
+    }
+
+    for(int j = 4; j < entradas; j++)
+    {
+        printf(" %f", entrada[0][j]);
+    }
+
+
     return 0;
 }
