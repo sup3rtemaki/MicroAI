fa6e74c34e5e4b945ea379d73272185683bb5be3
Celso Junior <celsofer93@gmail.com>
2020-11-04 22:43:34 -0300

    Inicio Implementacao da Rede

--- Teste_MLPerceptron/main.c
+++ Teste_MLPerceptron/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+//Multiplica matriz
+void multiplica_matriz(int A[][], int B[][], int C[][], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -24,6 +41,7 @@ int main()
     int entradas = 256;
     int qtd_neuronios = 200;
     int amostras_por_digito = 5;
+    int qtd_digitos = 10;
     int tam_vetor_saida = 10; //10 algarismos
     int amostras = amostras_por_digito * tam_vetor_saida;
     float limiar = 0.0;
@@ -35,11 +53,29 @@ int main()
     float w_anterior[qtd_neuronios]{tam_vetor_saida];
     float w0_anterior[1][tam_vetor_saida];
 
+    /* --- MATRIZES DE ATUALIZACAO DE PESOS E VALORES DE SAIDA DA REDE ---*/
+    float v_novo[entradas][qtd_neuronios];
+    float v0_novo[1][qtd_neuronios];
+    float w_novo[qtd_neuronios][tam_vetor_saida];
+    float w0_novo[1][tam_vetor_saida];
+    float z_inicial[1][qtd_neuronios];
+    float z[1][qtd_neuronios];
+    float deltinha_k[tam_vetor_saida][1];
+    float delta_w0[tam_vetor_saida][1];
+    float deltinha[1][qtd_neuronios];
+    float deltinha2[qtd_neuronios][1];
+    float entrada_aux[1][entradas];
+    float h[tam_vetor_saida][1];
+    float target[tam_vetor_saida][1];
+    int ciclo = 0;
+    float erro_total = 10000;
+
+
     float aleatorio = 0.2;
 
     /* --- ARQUIVO DE AMOSTRAS DE TREINAMENTO ---*/
     float entrada[amostras][entradas];
-    float target[10][tam_vetor_saida];
+    float target[qtd_digitos][tam_vetor_saida];
 
     for(int i = 0; i < amostras; i++)
     {
@@ -48,7 +84,6 @@ int main()
             entrada[i][j] = -1.0;
         }
     }
-
 
     while(c != EOF)
     {
@@ -93,7 +128,7 @@ int main()
     /*===========*/
 
     /* --- PREENCHIMENTO MATRIZ TARGET ---*/
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < qtd_digitos; i++)
     {
         for(int j = 0; j < tam_vetor_saida; j++)
         {
@@ -148,8 +183,19 @@ int main()
         w0_anterior[0][j] = (aleatorio*2) * (float)rand() / (float)RAND_MAX - aleatorio;
     }
 
-    /* --- MATRIZES DE ATUALIZACAO DE PESOS E VALORES DE SAIDA DA REDE ---*/
 
+    /* --- IMPLEMENTACAO DA REDE ---*/
+    while(erro_tolerado < erro_total)
+    {
+        erro_total = 0;
+        for(int padrao = 0; padrao < amostras; padrao++)
+        {
+            for(int j = 0; j < qtd_neuronios; j++)
+            {
+                z_inicial[0][j] =
+            }
+        }
+    }
 
     return 0;
 }
